d9d4764f590174d4130c69bc1a3acdf5fa2018fd
shawon <shawonislam121009@gmail.com>
2021-06-23 12:20:46 +0600

    saving user

--- app_authentication/views.py
+++ app_authentication/views.py
@@ -1,10 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from . serializers import SignUpSerializer
 
 
 @api_view(['POST'])
 def user_signup(request):
     if request.method == 'POST':
         data = request.data
-
+        serializer = SignUpSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            
         return Response(data={'result':'ok'})
