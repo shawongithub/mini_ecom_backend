fbad50ebf2a30e42aed7bbb0e2938f1195aa8c76
shawon <shawonislam121009@gmail.com>
2021-06-23 14:48:24 +0600

    added signup and login method

--- app_authentication/views.py
+++ app_authentication/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from . serializers import SignUpSerializer
-
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 @api_view(['POST'])
 def user_signup(request):
@@ -12,3 +13,17 @@ def user_signup(request):
             serializer.save()
             
         return Response(data={'result':'ok'})
+
+@api_view(['POST'])
+def user_login(request):
+    if request.method == 'POST':
+        data = request.data
+        username = data.get('username')
+        password = data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            print(user)
+        else:
+            print('not found')
+
+        return Response(data='ok')
