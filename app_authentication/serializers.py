215ea174e3f9c0781edba0f1ae695a964b2a1a72
shawon <shawonislam121009@gmail.com>
2021-06-23 11:56:39 +0600

    simplifying user serializer

--- app_authentication/serializers.py
+++ app_authentication/serializers.py
@@ -3,20 +3,15 @@ from django.contrib.auth.models import User
 
 
 class SignUpSerializer(serializers.ModelSerializer):
-    password2=serializers.CharField(style={'input_type':'password'},write_only=True)
+
     class Meta:
         model=User
-        fields=['username','password','password2']
-        extra_kwargs={
-            'password':{'write_only':True}
-        }
+        fields=['username','password']
+
     def save(self):
         account=User(
             username=self.validated_data['username']
         )
         password=self.validated_data['password']
-        password2=self.validated_data['password2']
-        if password !=password2:
-            raise serialization.ValidationError({'password':'password must match'})
         account.set_password(password)
         account.save()
--- app_authentication/views.py
+++ app_authentication/views.py
@@ -2,7 +2,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def user_signup(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
+        data = request.data
+
         return Response(data={'result':'ok'})
