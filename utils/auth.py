2a79b5d2e3475572a60ea6a507640723b15edce6
shawon <shawonislam121009@gmail.com>
2021-06-24 07:36:44 +0600

    added decoded token method

--- utils/auth.py
+++ utils/auth.py
@@ -13,6 +13,16 @@ class TokenProvider:
                 "exp":datetime.utcnow()+timedelta(seconds=self.jwt_time_validity_in_seconds)
             }
             jwt_token = jwt.encode(payload,self.secret_key)
+            print(jwt_token)
             return 1, jwt_token
+
         except:
             return 0, None
+
+    def get_token_value(self, token):
+        try:
+            decoded_token = jwt.decode(token,self.secret_key)
+            user_pk = decoded_token.get('id')
+            return True, user_pk
+        except:
+            return False, None
