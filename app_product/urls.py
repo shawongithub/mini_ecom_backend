2f754d239ce75882701126648903abf55a8510a8
shawon <shawonislam121009@gmail.com>
2021-06-25 20:19:13 +0600

    added add to cart method

--- app_product/urls.py
+++ app_product/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api/v1/list/create/', views.ProductList.as_view(), name='product_list'),
-    path('api/v1/detail/<int:pk>/', views.ProductDetail.as_view(), name='product_detail')
+    path('api/v1/detail/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
+    path('api/v1/addtocart/', views.add_to_cart, name='add_to_cart')
 ]
--- app_product/views.py
+++ app_product/views.py
@@ -1,4 +1,7 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework.response import Response
 from . models import Product
 from . serializers import ProductSerializer
 
@@ -12,3 +15,8 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
 
 
+@api_view(['POST'])
+def add_to_cart(request):
+    data = request.data
+    print(data)
+    return Response("ok")
